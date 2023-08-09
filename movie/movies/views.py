c58b8f82e8ecf3d72dc83a6b17d23f95027141ba
Schulz Hauptmann <100137249+zabit923@users.noreply.github.com>
2023-08-09 13:12:19 +0300

    + templates

--- movie/movies/views.py
+++ movie/movies/views.py
@@ -9,4 +9,4 @@ from .models import Movie
 class MovieView(View):
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, 'movies/movie_list.html', {'movie_list': movies})
+        return render(request, 'movies/movies.html', {'movie_list': movies})
