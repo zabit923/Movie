abf15c03ee404e9daaeabb5facc97f544315e153
Schulz Hauptmann <100137249+zabit923@users.noreply.github.com>
2023-08-14 20:46:18 +0300

    + Update reviews

--- movie/movies/admin.py
+++ movie/movies/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'id')
+    list_display_links = ('name',)
 
-admin.site.register(Category)
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre)
 admin.site.register(Movie)
 admin.site.register(MovieShots)
