4a0df29b35890e62729956b859a3522923185f28
Schulz Hauptmann <100137249+zabit923@users.noreply.github.com>
2023-08-19 23:39:57 +0300

    + Actor_detail

--- movie/movies/urls.py
+++ movie/movies/urls.py
@@ -8,5 +8,6 @@ from . import views
 urlpatterns = [
     path('', views.MovieView.as_view()),
     path('<slug:slug>/', views.MovieDetailViews.as_view(), name='movie_detail'),
-    path('review/<int:pk>/', views.AddReview.as_view(), name='add_review')
+    path('review/<int:pk>/', views.AddReview.as_view(), name='add_review'),
+    path('actor/<str:slug>/', views.ActorView.as_view(), name='actor_detail')
 ]
--- movie/movies/views.py
+++ movie/movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Movie, Category
+from .models import Movie, Actor
 from .forms import ReviewForm
 
 
@@ -29,3 +29,9 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+
+class ActorView(DetailView):
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = 'name'
